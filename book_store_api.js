4357f7928f63d240629fbbf04ae86b1613fae3b5
lavifre333 <lavifre333@gmail.com>
2023-05-01 22:44:00 +0300

    Create a REST server with node.js and express. Create a jest test file for the REST server.

---
+++ book_store_api.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/entities', (req, res) => {
+    // Read the content of the jeson file
+    const books_store_data = require('./Data/books_list.json')
+    // Send a Jeson response to the client
+    res.json(books_store_data)
+})
+
+app.listen(port, () => {
+    console.log(`book store app listening on port ${port}`)
+})
